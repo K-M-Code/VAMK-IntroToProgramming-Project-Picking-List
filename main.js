75d1864eb85075577e0717580769845e52d823c1
Mykola Zhabko <kokozhansei@gmail.com>
2020-11-20 23:23:54 +0200

    filder by Urder ID is ready

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function fetchOrder() {
+function fetchOrder(arg) {
 
     // var urlJson = "http://www.cc.puv.fi/~e2000594/VAMK-Project-Picking-List/project.json";
     var urlJson = "project.json";
@@ -8,10 +8,29 @@ function fetchOrder() {
         .then(res => res.json())
         .then(data => {
             var table = getTableTitles();
-            for (var i = 0; i < data.length; i++) {
-                table += fillTable(data[i]);
+            if (arg == 0 || document.getElementById("orderid").value == "") {
+                for (var i = 0; i < data.length; i++) {
+                    table += fillTable(data[i]);
+                }
+                document.getElementById("table_main").innerHTML = table;
+            } else if (arg == 1) {
+                console.log(arg);
+                var orderid = Number(document.getElementById("orderid").value);
+                for (var i = 0; i < data.length; i++) {
+                    console.log(data[i].orderid);
+                    var regex = RegExp("^" + orderid);
+                    if (regex.test(data[i].orderid)) {
+                        console.log(regex);
+                        if (regex == "/^/") {
+                            fetchOrder(0);
+                            break;
+                        }
+                        console.log("here");
+                        table += fillTable(data[i]);
+                    }
+                }
+                document.getElementById("table_main").innerHTML = table;
             }
-            document.getElementById("table_main").innerHTML = table;
         });
 
 
@@ -37,4 +56,4 @@ function getTableTitles() {
     return "<th>Order ID</th><th>Customer ID</th><th>Customer</th><th>Invoice address</th><th>Delivery address</th><th>Delivery date</th><th>Responsible sale person</th><th>Comment</th><th>Total price</th><th>Products</th></.>";
 
 }
-fetchOrder();
+fetchOrder(0);
