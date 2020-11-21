22307e785df0b434b638e704b4a202a6d9d51531
Mykola Zhabko <kokozhansei@gmail.com>
2020-11-21 19:50:19 +0200

    customer ID and customer name added and order list as a table for now

--- main.js
+++ main.js
@@ -8,25 +8,34 @@ function fetchOrder(arg) {
         .then(res => res.json())
         .then(data => {
             var table = getTableTitles();
-            if (arg == 0 || document.getElementById("orderid").value == "") {
+            var orderid = document.getElementById("orderid").value;
+            var customerid = document.getElementById("customerid").value;
+            var customer = document.getElementById("customer").value.toLowerCase();
+
+            var orderidRegex = RegExp("^" + orderid);
+            var customeridRegex = RegExp("^" + customerid);
+            var customerRegex = RegExp("^" + customer);
+
+            console.log(arg);
+
+            // console.log(orderidRegex);
+            // console.log(customerRegex);
+
+            if (arg == 0 && document.getElementById("orderid").value == "") {
                 for (var i = 0; i < data.length; i++) {
                     table += fillTable(data[i]);
                 }
                 document.getElementById("table_main").innerHTML = table;
-            } else if (arg == 1) {
-                console.log(arg);
-                var orderid = Number(document.getElementById("orderid").value);
+            } else if (arg == 1 || arg == 2 || arg == 3) {
+                console.log("here");
                 for (var i = 0; i < data.length; i++) {
-                    console.log(data[i].orderid);
-                    var regex = RegExp("^" + orderid);
-                    if (regex.test(data[i].orderid)) {
-                        console.log(regex);
-                        if (regex == "/^/") {
-                            fetchOrder(0);
-                            break;
-                        }
-                        console.log("here");
+                    // console.log(orderidRegex.test(data[i].orderid));
+                    // console.log(customeridRegex.test(data[i].customerid));
+                    // console.log(customerRegex.test(data[i].customer.toLowerCase()));
+                    if (orderidRegex.test(data[i].orderid) && customeridRegex.test(data[i].customerid) && customerRegex.test(data[i].customer.toLowerCase())) {
+
                         table += fillTable(data[i]);
+                        // console.log(customerRegex.test(data[i].customer.toLowerCase()));
                     }
                 }
                 document.getElementById("table_main").innerHTML = table;
@@ -43,7 +52,21 @@ function fillTable(data) {
     var tableString = "<tr>";
     if (data != 0) {
         for (var key in data) {
-            tableString += "<td>" + data[key] + "</td>"
+            if (Array.isArray(data[key])) {
+                tableString += "<td><table class=\"inner\"><th>code</th><th>product</th><th>description</th><th>suppliercode</th><th>qty</th><th>unit_price</th><th>shelf_pos</th><tr>";
+                for (var obj in data[key]) {
+                    for (var inner in data[key][obj]) {
+                        tableString += "<td>" + data[key][obj][inner] + "</td>";
+                    }
+                    tableString += "</tr>";
+                    // var list = JSON.parse(data[key][obj]);
+
+                    // tableString += "<li>" + data[key][obj].code + ": " + "</li>";
+                }
+                tableString += "</table></td>";
+            } else {
+                tableString += "<td>" + data[key] + "</td>";
+            }
         }
         tableString += "</tr>";
         return tableString;
